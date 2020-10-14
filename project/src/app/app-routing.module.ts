e13d44ecd73874f40611e9ba40196b76f07949a3
Débora <deboracristinapereira123@gmail.com>
2020-10-14 05:31:31 -0300

    Router e html componente principal

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreatePlayerComponent } from './player/create-player/create-player.component';
 
-const routes: Routes = [];
+/* PathMatch full: toda a URL. Path register: direciona para o formulário. */
+
+const routes: Routes = [
+  { path: 'register', component: CreatePlayerComponent },
+  { path: '', redirectTo: 'player', pathMatch: 'full' }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
