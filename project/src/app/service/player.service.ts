d430565248014aaae651c7b4732ff47be286a9ec
Débora <deboracristinapereira123@gmail.com>
2020-10-18 03:21:05 -0300

    Ajustes urls

--- project/src/app/service/player.service.ts
+++ project/src/app/service/player.service.ts
@@ -10,21 +10,21 @@ export class PlayerService {
 
   constructor(private http: HttpClient) { }
 
-  private baseUrl: string = 'http://localhost:8080/api/v1/players';
+  private baseUrl: string = 'http://localhost:8080/api/v1/';
  
   // Métodos get, post, etc. com base no endpoint e tendo como resposta a classe criada 
 
   // Observable: retorno da api (objeto Player)
 
   getPlayers() : Observable<Player[]> {
-    return this.http.get<Player[]>(this.baseUrl);
+    return this.http.get<Player[]>(this.baseUrl + "players");
   }
 
   createPlayer(player: Player) : Observable<Object> {
-    return this.http.post(this.baseUrl, player);
+    return this.http.post(this.baseUrl + "register", player);
   }
 
   deletePlayer(id: number): Observable<any> {
-    return this.http.delete(this.baseUrl + "/" + id);
+    return this.http.delete(this.baseUrl + "/delete/" + id);
   }
 }
--- projectBackend/src/main/java/com/simplus/projectBackend/controller/PlayerController.java
+++ projectBackend/src/main/java/com/simplus/projectBackend/controller/PlayerController.java
@@ -4,13 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-=======
-import org.springframework.web.bind.annotation.GetMapping;
->>>>>>> 560f2059b1ea62fcd650eb8d406ba94700e26735
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,20 +40,17 @@ public class PlayerController {
 	
 	// Post: salva o objeto no banco de dados e retorna esse objeto
 	
-	@PostMapping("/players")
+	@PostMapping("/register")
 	public Player createPlayer(@RequestBody Player player) {
 		return this.playerRepository.save(player);
 	}
-<<<<<<< HEAD
 	
 	// Delete: deletar aquele com o id e retornar lista de jogadores 
 	
-	@DeleteMapping("/players/{id}")
+	@DeleteMapping("/delete/{id}")
 	public List<Player> deletePlayer(@PathVariable Long id) {
 		this.playerRepository.deleteById(id);
 		return this.playerRepository.findAll();
 	}
-=======
->>>>>>> 560f2059b1ea62fcd650eb8d406ba94700e26735
 
 }
